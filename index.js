3629d54fe14d13f18f578894fd00da74275faf2a
ikromulabid00 <ikromul_abid@teknokrat.ac.id>
2024-08-30 23:16:57 +0700

    feat: add notes

--- index.js
+++ index.js
@@ -8,3 +8,19 @@ class Animal {
         console.log(`says` + this.sound);
     }
 }
+
+// git init -> menginisialisasi prject git baru
+
+// git add -> untuk menambahkan file kedalam staging area
+
+// git commit -m (commit message) -> menambahkan file kedalam repository
+
+// git status -> untuk melihat status perubahan
+
+// git remote add oriign -> untuk menambahkan link remote repository
+
+// git remote -v -> melihat remote yang ada
+
+// git push -> untuk upload dari local repository ke remote repository 
+
+// git pull -> download dari remote repository ke local repository
